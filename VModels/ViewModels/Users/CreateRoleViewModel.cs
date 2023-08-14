58b8ebcbd810a716942e004394253f012889f849
i-radi <ibrahimmohammedradi@gmail.com>
2023-08-14 14:22:59 +0300

    Fix Manage Roles UI

--- VModels/ViewModels/Users/CreateRoleViewModel.cs
+++ VModels/ViewModels/Users/CreateRoleViewModel.cs
@@ -8,8 +8,11 @@ public class CreateRoleViewModel
     public string UserName { get; set; } = string.Empty;
     public string RoleName { get; set; } = string.Empty;
     public SelectList? RoleOptions { get; set; }
-    public int SchoolId { get; set; }
-    public SelectList? SchoolOptions { get; set; }
-    public int OrganizationId { get; set; }
+    public int? OrganizationId { get; set; }
     public SelectList? OrganizationOptions { get; set; }
+    public int? SchoolId { get; set; }
+    public SelectList? SchoolOptions { get; set; }
+
+    public int? ActivityId { get; set; }
+    public SelectList? ActivityOptions { get; set; }
 }
