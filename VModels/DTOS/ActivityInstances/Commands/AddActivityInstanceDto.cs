c7bc5f56801a98ff9a1f37dfdfb343c0f43212e5
i-radi <ibrahimmohammedradi@gmail.com>
2023-08-23 16:39:49 +0300

    Add ActivityInstances DTOs

---
+++ VModels/DTOS/ActivityInstances/Commands/AddActivityInstanceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VModels.DTOS;
+
+public class AddActivityInstanceDto
+{
+    public int ActivityId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ForDate { get; set; }
+    public bool IsLocked { get; set; }
+    public int SeasonId { get; set; }
+}
---
+++ VModels/DTOS/ActivityInstances/Queries/GetActivityInstanceDto.cs
@@ -0,0 +1,12 @@
+﻿namespace VModels.DTOS;
+
+public class GetActivityInstanceDto
+{
+    public int Id { get; set; }
+    public int ActivityId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ForDate { get; set; }
+    public bool IsLocked { get; set; }
+    public int SeasonId { get; set; }
+}
