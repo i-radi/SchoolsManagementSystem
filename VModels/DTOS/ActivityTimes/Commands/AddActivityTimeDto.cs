1f86bd0cf9a6b250b06c8a2c383115c8713f5c60
i-radi <ibrahimmohammedradi@gmail.com>
2023-08-23 16:40:10 +0300

    Add ActivityTimes DTOs

---
+++ VModels/DTOS/ActivityTimes/Commands/AddActivityTimeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VModels.DTOS;
+
+public class AddActivityTimeDto
+{
+    public int ActivityId { get; set; }
+    public string Day { get; set; } = string.Empty;
+    public DateTime FromTime { get; set; }
+    public DateTime ToTime { get; set; }
+    public string? Body { get; set; }
+}
