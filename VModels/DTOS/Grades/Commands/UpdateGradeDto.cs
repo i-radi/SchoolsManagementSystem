82624c05bdf44c427b8834409d9f4bf70c86a9be
i-radi <ibrahimmohammedradi@gmail.com>
2023-08-27 09:44:51 +0300

    update Grades UI

--- VModels/DTOS/Grades/Commands/UpdateGradeDto.cs
+++ VModels/DTOS/Grades/Commands/UpdateGradeDto.cs
@@ -4,5 +4,7 @@ public class UpdateGradeDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int Order { get; set; }
     public int SchoolId { get; set; }
 }
--- VModels/ViewModels/Grades/GradeViewModel.cs
+++ VModels/ViewModels/Grades/GradeViewModel.cs
@@ -4,6 +4,8 @@ public class GradeViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int Order { get; set; }
     public int SchoolId { get; set; }
     public virtual School? School { get; set; }
     public virtual ICollection<Classroom> Classrooms { get; set; } = new HashSet<Classroom>();
