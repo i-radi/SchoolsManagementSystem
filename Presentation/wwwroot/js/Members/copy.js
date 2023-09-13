e7cf4d91cab8ff1e1bac11c451add6f7447eb285
i-radi <ibrahimmohammedradi@gmail.com>
2023-09-13 14:12:39 +0300

    copy class members validations

--- Presentation/wwwroot/js/Members/copy.js
+++ Presentation/wwwroot/js/Members/copy.js
@@ -68,4 +68,74 @@
     $("#toSeasonId, #toClassroomId").change(function () {
         checkSelections();
     });
+
+    $('#toOrgId').on('change', function () {
+        var selectedOrganizationId = $(this).val();
+        var schoolDropdown = $('#toSchoolId');
+
+        schoolDropdown.empty();
+
+        schoolDropdown.append($('<option>').val('').text('Select School'));
+        $.ajax({
+            url: '/Users/GetSchoolsByOrganization',
+            type: 'GET',
+            data: { organizationId: selectedOrganizationId },
+            success: function (data) {
+                $.each(data, function (i, item) {
+                    schoolDropdown.append($('<option>').val(item.value).text(item.text));
+                });
+            }
+        });
+    });
+
+    $('#toSchoolId').on('change', function () {
+        var selectedSchoolId = $(this).val();
+
+        var gradeDropdown = $('#toGradeId');
+        gradeDropdown.empty();
+        gradeDropdown.append($('<option>').val('').text('Select Grade'));
+        $.ajax({
+            url: '/Users/GetGradesBySchool',
+            type: 'GET',
+            data: { schoolId: selectedSchoolId },
+            success: function (data) {
+                $.each(data, function (i, item) {
+                    gradeDropdown.append($('<option>').val(item.value).text(item.text));
+                });
+            }
+        });
+
+        var seasonDropdown = $('#toSeasonId');
+        seasonDropdown.empty();
+        seasonDropdown.append($('<option>').val('').text('Select Season'));
+        $.ajax({
+            url: '/Users/GetSeasonsBySchool',
+            type: 'GET',
+            data: { schoolId: selectedSchoolId },
+            success: function (data) {
+                $.each(data, function (i, item) {
+                    seasonDropdown.append($('<option>').val(item.value).text(item.text));
+                });
+            }
+        });
+    });
+
+    $('#toGradeId').on('change', function () {
+        var selectedGradeId = $(this).val();
+        var classroomDropdown = $('#toClassroomId');
+
+        classroomDropdown.empty();
+
+        classroomDropdown.append($('<option>').val('').text('Select Classroom'));
+        $.ajax({
+            url: '/Users/GetClassroomsByGrade',
+            type: 'GET',
+            data: { gradeId: selectedGradeId },
+            success: function (data) {
+                $.each(data, function (i, item) {
+                    classroomDropdown.append($('<option>').val(item.value).text(item.text));
+                });
+            }
+        });
+    });
 });
