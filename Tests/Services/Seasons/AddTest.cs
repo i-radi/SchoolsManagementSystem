f3b5a8903c1a176febaca41649ac99c2e579b4f6
i-radi <ibrahimmohammedradi@gmail.com>
2023-09-26 14:57:22 +0300

    Add seasons unit tests

---
+++ Tests/Services/Seasons/AddTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Models.Entities;
+
+namespace Tests.Services.Seasons;
+
+public class AddTest
+{
+    private readonly IMapper _mapperMock;
+    private readonly Mock<ISeasonRepo> _seasonRepoMock;
+    private readonly SeasonService _seasonService;
+
+    public AddTest()
+    {
+        _mapperMock = MapperMock.GetAllProfile();
+        _seasonRepoMock = new();
+        _seasonService = new(_seasonRepoMock.Object, _mapperMock);
+    }
+
+    [Fact]
+    public async Task Add_ValidItem_ReturnsItem()
+    {
+        //Arrange
+        var seasonDto = new AddSeasonDto() { Name = "Cairo season" };
+        var season = _mapperMock.Map<Season>(seasonDto);
+
+        _seasonRepoMock.Setup(x => x.AddAsync(season)).Returns(Task.FromResult(season));
+
+        //Act
+        var result = await _seasonService.Add(seasonDto);
+
+        //Assert
+        result.Data.Should().NotBeNull();
+        result.Succeeded.Should().BeTrue();
+        result.Data.Should().BeOfType<GetSeasonDto>();
+        result.Data.Name.Should().BeEquivalentTo("Cairo season");
+    }
+
+}
--- VModels/DTOS/Seasons/Queries/GetSeasonDto.cs
+++ VModels/DTOS/Seasons/Queries/GetSeasonDto.cs
@@ -3,6 +3,7 @@
 public class GetSeasonDto
 {
     public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime From { get; set; }
     public DateTime To { get; set; }
     public bool IsCurrent { get; set; }
