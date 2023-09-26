314886d5be232851761c91d513894a7034962695
i-radi <ibrahimmohammedradi@gmail.com>
2023-09-26 16:11:00 +0300

    Add Activities unit tests

---
+++ Tests/Services/Activities/AddTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Models.Entities;
+
+namespace Tests.Services.Activities;
+
+public class AddTest
+{
+    private readonly IMapper _mapperMock;
+    private readonly Mock<IActivityRepo> _activityRepoMock;
+    private readonly ActivityService _activityService;
+
+    public AddTest()
+    {
+        _mapperMock = MapperMock.GetAllProfile();
+        _activityRepoMock = new();
+        _activityService = new(_activityRepoMock.Object, _mapperMock);
+    }
+
+    [Fact]
+    public async Task Add_ValidItem_ReturnsItem()
+    {
+        //Arrange
+        var activityDto = new AddActivityDto() { Name = "Cairo activity" };
+        var activity = _mapperMock.Map<Activity>(activityDto);
+
+        _activityRepoMock.Setup(x => x.AddAsync(activity)).Returns(Task.FromResult(activity));
+
+        //Act
+        var result = await _activityService.Add(activityDto);
+
+        //Assert
+        result.Data.Should().NotBeNull();
+        result.Succeeded.Should().BeTrue();
+        result.Data.Should().BeOfType<GetActivityDto>();
+        result.Data.Name.Should().BeEquivalentTo("Cairo activity");
+    }
+
+}
---
+++ Tests/Services/Activities/UpdateTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Models.Entities;
+
+namespace Tests.Services.Activities;
+
+public class UpdateTest
+{
+    private readonly IMapper _mapperMock;
+    private readonly Mock<IActivityRepo> _activityRepoMock;
+    private readonly ActivityService _activityService;
+
+    public UpdateTest()
+    {
+        _mapperMock = MapperMock.GetAllProfile();
+        _activityRepoMock = new();
+        _activityService = new(_activityRepoMock.Object, _mapperMock);
+    }
+
+
+    [Theory]
+    [InlineData(1)]
+    public async Task Update_ValidItem_ReturnsSuccess(int id)
+    {
+        //Arrange
+        var activity = new Activity() { Id = id, Name = "Cairo Activity" };
+        _activityRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult(activity));
+
+        var activityDto = new UpdateActivityDto() { Id = id, Name = "Alex Activity" };
+
+        //Act
+        var result = await _activityService.Update(activityDto);
+
+        //Assert
+        result.Succeeded.Should().BeTrue();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        result.Data.Should().BeTrue();
+        _activityRepoMock.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Once, "Not Called");
+    }
+
+    [Theory]
+    [InlineData(5)]
+    public async Task Update_InValidItem_ReturnsNotFound(int id)
+    {
+        //Arrange
+        _activityRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult<Activity>(null));
+
+        var activityDto = new UpdateActivityDto() { Id = id, Name = "Alex Activity" };
+
+        //Act
+        var result = await _activityService.Update(activityDto);
+
+        //Assert
+        result.Succeeded.Should().BeFalse();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        result.Data.Should().BeFalse();
+        _activityRepoMock.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Once, "Not Called");
+    }
+}
