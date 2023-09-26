993b3bfc24242e8f0899ce48402887b2608344ae
i-radi <ibrahimmohammedradi@gmail.com>
2023-09-26 15:29:54 +0300

    Add Courses unit tests

---
+++ Tests/Services/Courses/GetByIdTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Models.Entities;
+using Tests.PassData;
+
+namespace Tests.Services.Courses;
+
+public class GetByIdTest
+{
+    private readonly IMapper _mapperMock;
+    private readonly Mock<ICourseRepo> _courseRepoMock;
+    private readonly CourseService _courseService;
+
+    public GetByIdTest()
+    {
+        _mapperMock = MapperMock.GetAllProfile();
+        _courseRepoMock = new();
+        _courseService = new(_courseRepoMock.Object, _mapperMock);
+    }
+
+    [Theory]
+    [MemberData(nameof(PassDataToParamUsingMemberData.GetParamData), MemberType = typeof(PassDataToParamUsingMemberData))]
+    public async void GetById_where_Found_Return_StatusCode200(int id)
+    {
+        //Arrange
+        var course = new Course() { Id = id, Name = "Cairo course" };
+
+        _courseRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult(course));
+
+        //Act
+        var result = await _courseService.GetById(id);
+
+        //Assert
+        result.Data.Should().NotBeNull();
+        result.Succeeded.Should().BeTrue();
+        result.Data.Should().BeOfType<GetCourseDto>();
+    }
+
+    [Theory]
+    [InlineData(5)]
+    public async void GetById_where_NotFound_Return_Null(int id)
+    {
+        //Arrange
+        _courseRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult<Course>(null));
+
+        //Act
+        var result = await _courseService.GetById(id);
+
+        //Assert
+        result.Should().BeNull();
+    }
+}
---
+++ Tests/Services/Courses/UpdateTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Models.Entities;
+
+namespace Tests.Services.Courses;
+
+public class UpdateTest
+{
+    private readonly IMapper _mapperMock;
+    private readonly Mock<ICourseRepo> _courseRepoMock;
+    private readonly CourseService _courseService;
+
+    public UpdateTest()
+    {
+        _mapperMock = MapperMock.GetAllProfile();
+        _courseRepoMock = new();
+        _courseService = new(_courseRepoMock.Object, _mapperMock);
+    }
+
+
+    [Theory]
+    [InlineData(1)]
+    public async Task Update_ValidItem_ReturnsSuccess(int id)
+    {
+        //Arrange
+        var course = new Course() { Id = id, Name = "Cairo Course" };
+        _courseRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult(course));
+
+        var courseDto = new UpdateCourseDto() { Id = id, Name = "Alex Course" };
+
+        //Act
+        var result = await _courseService.Update(courseDto);
+
+        //Assert
+        result.Succeeded.Should().BeTrue();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        result.Data.Should().BeTrue();
+        _courseRepoMock.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Once, "Not Called");
+    }
+
+    [Theory]
+    [InlineData(5)]
+    public async Task Update_InValidItem_ReturnsNotFound(int id)
+    {
+        //Arrange
+        _courseRepoMock.Setup(x => x.GetByIdAsync(id)).Returns(Task.FromResult<Course>(null));
+
+        var courseDto = new UpdateCourseDto() { Id = id, Name = "Alex Course" };
+
+        //Act
+        var result = await _courseService.Update(courseDto);
+
+        //Assert
+        result.Succeeded.Should().BeFalse();
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+        result.Data.Should().BeFalse();
+        _courseRepoMock.Verify(x => x.GetByIdAsync(It.IsAny<int>()), Times.Once, "Not Called");
+    }
+}
